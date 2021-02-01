12a949f557f64f0ebfe3bc7324bfb91b7435e397
Olga Shenaeva <shenaeva.olga@yandex.ru>
2021-02-01 15:57:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceBeforeOperation = 2_000_000_000;
+        int operation = 500_000_000;
+        int balanceAfterOperation = balanceBeforeOperation + operation;
+        System.out.println (balanceAfterOperation);
+    }
+
+}
